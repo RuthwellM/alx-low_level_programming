85e2a243ea81b695f7f41c141dc3bc9069a54e26
RuthwellM <78424837+RuthwellM@users.noreply.github.com>
2022-03-16 07:47:47 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,9 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
-
-	while (i < 24)
+	for (i =0; i <= 23; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (j = 0; j <= 59; j++)
 		{		
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
